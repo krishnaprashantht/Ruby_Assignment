0409f675be7bfd779ce9f09adb664dbf5ac28aae
Krishna Prashanth T <krishnaprashanth.t@tcs.com>
2013-03-26 23:13:58 +0530

    Initial commit of the workspace

---
+++ proj_three/app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+  def index
+
+	puts "Inside Welcome Controller"	
+
+  end
+end
---
+++ proj_three/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjThree::Application.config.secret_token = '52f319639077fd7a3c451edd6400b7379c596e2defdc3251b90f2b970994005adf341be7433f92e7b5901a24174f6486b9db5cf2706e43a5dd762c15469fff4d'
